a600fba28b87573c5f38bdfa52e6e90b89a919e9
Oscar Love <olabradalove@gmail.com>
2023-03-19 19:11:43 +0000

    Fixed Client

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateBoardComponent } from './components/create-board/create_board.component';
 
 const routes: Routes = [
+    { path: '', component: CreateBoardComponent }
 ];
 
 @NgModule({
