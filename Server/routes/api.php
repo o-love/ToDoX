c332e865440c99e07abc5954104e73d1134fbf94
Oscar Love <olabradalove@gmail.com>
2023-03-20 15:17:45 +0000

    Temp commit

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BoardController;
@@ -17,6 +18,9 @@ use App\Http\Controllers\BoardController;
 
 Route::get('board', [BoardController::class, 'index']);
 Route::post('board', [BoardController::class, 'create']);
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
