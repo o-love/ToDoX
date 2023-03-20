d816799071f4b725dd40372eff3185aa422052e7
me <me>
2023-03-20 13:34:18 +0000

    Fixed board component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateBoardComponent } from './components/create-board/create_board.component';
+import { CreateBoardComponent } from './components/create-board/create-board.component';
 import { BoardListComponent } from './components/board-list/board-list.component';
 
 const routes: Routes = [
