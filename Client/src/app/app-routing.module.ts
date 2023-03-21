3ba3215fc8321c7ceacd51c98b47a033945bd280
me <me>
2023-03-21 23:26:54 +0000

    Added boar detail, list creation and board list - missing user interaction on list creation

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateBoardComponent } from './components/create-board/create-board.component';
 import { BoardListComponent } from './components/board-list/board-list.component';
+import { BoardDetailComponent } from './components/board-detail/board-detail.component';
 
 const routes: Routes = [
     { path: '', component: CreateBoardComponent },
