c453f0481f6899107e08a1473ea94d6572a242d7
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-04-16 00:45:02 +0100

    Improved style for create board

--- Client/src/app/components/create-board/create-board.component.ts
+++ Client/src/app/components/create-board/create-board.component.ts
@@ -1,17 +1,25 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { BoardService } from 'src/app/services/board-taskList-service/board-taskList-service.service';
 import { Board } from 'src/app/models/board';
-
-// This component will be responsible for creating boards for a user.
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Form } from 'src/app/models/form';
 
 @Component({
   selector: 'app-create-board',
   templateUrl: './create-board.component.html',
   styleUrls: ['./create-board.component.scss']
 })
+export class CreateBoardComponent implements Form {
+  form: FormGroup;
 
-export class CreateBoardComponent {
-  constructor(private boardService: BoardService) { }
+  @ViewChild('name') name!: ElementRef<any>; 
+
+  constructor(private boardService: BoardService, private fb: FormBuilder) { 
+    this.form = this.fb.group({
+      boardName: ['', Validators.required],
+      boardDescription: ['']
+    })
+  }
 
   boardName: string = '';
   boardDescription: string = '';
@@ -21,6 +29,8 @@ export class CreateBoardComponent {
 
   onSubmit() {
     console.log("board: ", this.boardName, " description: ", this.boardDescription)
+    if (this.checkErrors()) return;
+    
     this.boardService.createBoard(this.boardName, this.boardDescription).subscribe({
       next: (board) => {
         this.boardCreated.emit(board);
@@ -38,5 +48,29 @@ export class CreateBoardComponent {
 
   onClose() {
     this.closePopup.emit();
+  }
+
+  checkErrors(): boolean {
+    if (this.form.get('boardName')?.errors) {
+      this.onError(this.name);
+      return true;
+    }  
+    return false;
+  }
+
+  resetErrors(): void {
+    this.name.nativeElement.style.boxShadow = 'none';
+  }
+  
+  onError(label: ElementRef) {
+    label.nativeElement.style.boxShadow = '0px 0px 7px rgba(255, 113, 113, 0.7)';
+  }
+
+  onFocus(event: any, label: any) {
+    label.classList.add('focused');
+  }
+
+  onBlur(event: any, label: any) {
+    if (!event.target.value) label.classList.remove('focused');
   }
 }
