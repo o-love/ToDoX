210d31c2042978d6446dce1b73687bd3c95315c0
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-04-16 19:25:13 +0100

    Improved create list popup

--- Client/src/app/components/create-list/create-list.component.ts
+++ Client/src/app/components/create-list/create-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { BoardService } from 'src/app/services/board-taskList-service/board-taskList-service.service';
 import { TaskList } from 'src/app/models/taskList';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Form } from 'src/app/models/form';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 // This component will be responsible for creating lists for a user on a board.
 
@@ -10,18 +12,54 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './create-list.component.html',
   styleUrls: ['./create-list.component.scss']
 })
-export class CreateListComponent {
-  constructor(private boardService: BoardService, private route: ActivatedRoute, private router: Router) {}
-
+export class CreateListComponent implements Form {
+  form: FormGroup;
   listName: string = '';
   listDescription: string = '';
   boardId = this.route.snapshot.paramMap.get('boardId');
   stateIds: number[] = [];
-  
+
   @Output() listCreated = new EventEmitter<any>();
   @Output() closePopup = new EventEmitter<void>();
 
+  @ViewChild('name') name!: ElementRef<any>;
+
+  constructor(private fb: FormBuilder, private boardService: BoardService, private route: ActivatedRoute, private router: Router) {
+    this.form = this.fb.group({
+      listName: ['', [Validators.required]],
+      listDescription: ['', []]
+    });
+  }
+
+  checkErrors(): boolean {
+    if (this.form.controls['listName'].errors) {
+      this.onError(this.name);
+      return true;
+    }  
+    return false;
+  }
+
+  resetErrors(): void {
+    this.name.nativeElement.style.boxShadow = 'none';
+  }
+  
+  onError(label: ElementRef) {
+    label.nativeElement.style.boxShadow = '0px 0px 7px rgba(255, 113, 113, 0.7)';
+  }
+
+  onFocus(event: any, label: any) {
+    label.classList.add('focused');
+  }
+
+  onBlur(event: any, label: any) {
+    if (!event.target.value) label.classList.remove('focused');
+  }
+
   onSubmit() {
+    console.log("list: ", this.listName, " description: ", this.listDescription);
+    this.resetErrors();
+    if (this.checkErrors()) return;
+
     if (this.boardId && this.listName) {
       this.boardService.createList(this.boardId, this.listName, this.listDescription, this.stateIds).subscribe({
         next: (list: TaskList) => {
