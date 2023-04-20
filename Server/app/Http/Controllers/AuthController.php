9bf2cbee627af4de46d38689172c3422393731e4
Oscar Love <olabradalove@gmail.com>
2023-04-20 09:57:21 +0100

    Basic Auth Controller temp

--- Server/app/Http/Controllers/AuthController.php
+++ Server/app/Http/Controllers/AuthController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
 
+        if (Auth::attempt(array('email' => $credentials["email"], 'password' => $credentials["password"]), true))
+        {
+
+        }
     }
 }
