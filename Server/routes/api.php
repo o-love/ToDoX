a7ad0d087223b89379d8bc1cefc002cfaccafa8a
Oscar Love <olabradalove@gmail.com>
2023-04-22 22:02:22 +0100

    Successful token creation after validating user

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -65,5 +65,7 @@ Route::post('labels', [LabelController::class, 'store'])->name('labels.store');
 
 Route::apiResource('users', \App\Http\Controllers\UserController::class);
 
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'store']);
+
 // Route::resource('boards', BoardController::class); // Boards resource routes - resftful
 // Route::resource('boards.lists', BoardListController::class)->shallow(); // Lists resource routes - resftful
