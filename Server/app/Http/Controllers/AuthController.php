91e885626a59bef636128345ea0a6fb08cdce7e9
Oscar Love <olabradalove@gmail.com>
2023-04-23 09:31:52 +0100

    Auth light touches

--- Server/app/Http/Controllers/AuthController.php
+++ Server/app/Http/Controllers/AuthController.php
@@ -4,23 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function store(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+        $credentials = $request->only('email', 'password');
 
-        $user = User::where('email', $request->email)->first();
+        if (Auth::attempt($credentials, true))
+        {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response('Login invalid', 503);
+            return response()->json(['token' => $token]);
         }
-
-        return $user->createToken('auth_token')->plainTextToken;
+        else
+        {
+            return response()->json(['error' => 'Login Invalid'], 503);
+        }
     }
 }
--- Server/config/auth.php
+++ Server/config/auth.php
@@ -43,7 +43,7 @@ return [
         'api' => [
             'driver' => 'sanctum',
             'provider' => 'users',
-            'hash' => true,
+            'hash' => false,
         ],
     ],
 
